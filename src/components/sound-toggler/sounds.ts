e867f5027bf495d30f0f167c8983326a6be26506
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-07-16 10:31:44 +0300

    some sound fixes

--- src/components/sound-toggler/sounds.ts
+++ src/components/sound-toggler/sounds.ts
@@ -1,7 +1,7 @@
 import { SoundModel } from './sound.model';
 
 export default {
-  'menu': new SoundModel('/assets/music/menu.mp3'),
-  'door': new SoundModel('/assets/music/door.mp3'),
-  'characters': new SoundModel('/assets/music/characters.mp3'),
+  'menu': new SoundModel('assets/music/menu.mp3'),
+  'door': new SoundModel('assets/music/door.mp3'),
+  'characters': new SoundModel('assets/music/characters.mp3'),
 };
